f370f25946e3483560c8ac62ce8acc31f3130a38
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-10-26 15:25:24 -0400

    prototypes

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -14,3 +14,20 @@ test("creates a player object", () => {
     expect.arrayContaining([expect.any(Object)])
   );
 });
+
+test("get player's stats as an object", () => {
+  const player = new Player("Dave");
+
+  expect(player.getStats()).toHaveProperty("potions");
+  expect(player.getStats()).toHaveProperty("health");
+  expect(player.getStats()).toHaveProperty("strength");
+  expect(player.getStats()).toHaveProperty("agility");
+});
+
+test("get's inventory from player or returns false", () => {
+  const player = new Player("Dave");
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+  player.inventory = [];
+  expect(player.getInventory()).toEqual(false);
+});
